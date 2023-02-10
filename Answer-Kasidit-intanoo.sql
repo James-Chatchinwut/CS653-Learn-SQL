0583ddc039894200bc679739c49d935f0c71708f
Kasidit Intanoo <104260461+KasiditIntanoo@users.noreply.github.com>
2023-02-11 00:25:58 +0700

    Update Answer-Kasidit-intanoo.sql

--- Answer-Kasidit-intanoo.sql
+++ Answer-Kasidit-intanoo.sql
@@ -12,18 +12,20 @@
 UPDATE albums 
   SET release_year = 1986
   WHERE id = 4;
-SELECT * FROM albums ;
+SELECT * FROM albums ; -- to check year in albums id No.4
      
 INSERT INTO bands (name)
  VALUES ('Safeplanet');
+-- SELECT * FROM bands ; to check band_id before add albums
      
 INSERT INTO albums (name, release_year, band_id)
  VALUES ('Safeboys', 2019, 8);
 
 SELECT * FROM albums;
-
+-- SELECT * FROM albums ; to check albums id before delete albums.
 DELETE FROM albums    
- WHERE id = 19;    
+ WHERE id = 19;
+ -- SELECT * FROM bands ; to check albums id before delete bands.
  DELETE FROM bands
  WHERE id = 8;
  
