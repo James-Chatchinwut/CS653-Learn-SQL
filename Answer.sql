7a9fa77685e8c243dcb811adf2a2f66d773b4a64
Kasidit Intanoo <104260461+KasiditIntanoo@users.noreply.github.com>
2023-02-10 22:30:03 +0700

    Update comment

--- Answer.sql
+++ Answer.sql
@@ -1,6 +1,7 @@
 Answer
  
-1.1 CREATE TABLE songs(
+--1.1 
+ CREATE TABLE songs(
      id INT NOT NULL AUTO_INCREMENT,
      name VARCHAR(255) NOT NULL,
      length FLOAT NOT NULL,
@@ -8,46 +9,54 @@ Answer
      PRIMARY KEY (id),
     FOREIGN KEY (album_id) REFERENCES albums(id)
 );
-1.2 INSERT INTO songs(id,name,length,album_id) VALUES (1,'Arrival',1+(30/60),1); and more...
+--1.2 
+  INSERT INTO songs(id,name,length,album_id) 
+  VALUES (1,'Arrival',1+(30/60),1); and more...
 
-2.SELECT DISTINCT bands.name AS 'Band Name'
+--2.
+SELECT DISTINCT bands.name AS 'Band Name'
 FROM bands
 JOIN albums ON bands.id = albums.band_id;
 
-3.SELECT * FROM albums
+--3.
+SELECT * FROM albums
 WHERE release_year IS NOT NULL
 ORDER BY release_year
 LIMIT 1;
 
-4.SELECT DISTINCT bands.name AS 'Band Name'
+--4.
+SELECT DISTINCT bands.name AS 'Band Name'
 FROM bands
 JOIN albums ON bands.id = albums.band_id;
 
-5.
+--5.
 
-6.SELECT
-       albums.name as Name,
-       albums.release_year as 'Release Year',
-       SUM(songs.length) as 'Duration'
-     FROM albums
-     JOIN songs on albums.id = songs.album_id
-     GROUP BY songs.album_id
-     ORDER BY Duration DESC
-     LIMIT 1;
+--6.
+ SELECT
+   albums.name as Name,
+   albums.release_year as 'Release Year',
+   SUM(songs.length) as 'Duration'
+    FROM albums
+    JOIN songs on albums.id = songs.album_id
+    GROUP BY songs.album_id
+    ORDER BY Duration DESC
+    LIMIT 1;
 
-7.UPDATE albums 
+--7.
+  UPDATE albums 
      SET release_year = 1986
      WHERE id = 4;
 
-8. INSERT INTO bands (name)
-     VALUES ('Safeplanet');
-
-
+--8. 
+INSERT INTO bands (name)
+ VALUES ('Safeplanet');
+     
 INSERT INTO albums (name, release_year, band_id)
-VALUES ('Safeboys', 2019, 8);
+ VALUES ('Safeboys', 2019, 8);
 
 
-9.DELETE FROM albums    
-    WHERE id = 19;    
-    DELETE FROM bands
-    WHERE id = 8;
+--9.
+DELETE FROM albums    
+ WHERE id = 19;    
+ DELETE FROM bands
+ WHERE id = 8;
