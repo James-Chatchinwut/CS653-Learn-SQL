38d61613165892fd350ad884f3c9cf6f9f95b00a
Kasidit Intanoo <104260461+KasiditIntanoo@users.noreply.github.com>
2023-02-10 23:51:11 +0700

    remove comment

--- Answer-Kasidit-intanoo.sql
+++ Answer-Kasidit-intanoo.sql
@@ -1,6 +1,4 @@
---Answer by KASIDIT INTANOO
 
---6.
  SELECT
    albums.name as Name,
    albums.release_year as 'Release Year',
@@ -11,20 +9,16 @@
     ORDER BY Duration DESC
     LIMIT 1;
 
---7.
-  UPDATE albums 
-     SET release_year = 1986
-     WHERE id = 4;
-
---8. 
+UPDATE albums 
+  SET release_year = 1986
+  WHERE id = 4;
+     
 INSERT INTO bands (name)
  VALUES ('Safeplanet');
      
 INSERT INTO albums (name, release_year, band_id)
  VALUES ('Safeboys', 2019, 8);
 
-
---9.
 DELETE FROM albums    
  WHERE id = 19;    
  DELETE FROM bands
